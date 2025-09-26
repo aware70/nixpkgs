ad24c04d62906a3420ed55282a18c07ef5daea90
aware70 <7832566+aware70@users.noreply.github.com>
2025-09-26 17:45:43 -0500

    Refactor settings into separate files

--- nixos/modules/services/computing/flux-framework/broker.nix
+++ nixos/modules/services/computing/flux-framework/broker.nix
@@ -6,10 +6,7 @@
   ...
 }:
 let
-
   cfg = config.services.flux-broker;
-  systemCfg = cfg.system;
-  securityCfg = cfg.security;
   opt = options.services.flux-broker;
   toml = pkgs.formats.toml {};
   systemToml = toml.generate "flux-system.toml"
@@ -18,313 +15,6 @@ let
     (lib.attrsets.filterAttrsRecursive (name: value: value != null) cfg.security.sign.settings);
   impToml = toml.generate "flux-security-imp.toml"
     (lib.attrsets.filterAttrsRecursive (name: value: value != null) cfg.security.imp.settings);
-
-  systemOptions = {
-    systemd.enable = lib.mkOption {
-      internal = true;
-    };
-
-    exec = {
-      imp = lib.mkOption {
-        type = lib.types.path;
-        internal = true;
-      };
-
-      job-shell = lib.mkOption {
-        type = lib.types.path;
-        internal = true;
-      };
-
-      service = lib.mkOption {
-        type = lib.types.str;
-        internal = true;
-      };
-
-      sdexec-properties = lib.mkOption {
-        type = lib.types.attrs;
-        default = {
-          MemoryMax = "95%";
-        };
-        description = ''
-        '';
-      };
-    };
-
-    bootstrap = {
-      curve_cert = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
-        description = ''
-        '';
-      };
-      default_port = lib.mkOption {
-        type = lib.types.port;
-        default = 8050;
-        description = ''
-        '';
-      };
-      default_bind = lib.mkOption {
-        type = lib.types.str;
-        default = "tcp://eth1:%p";
-        description = ''
-        '';
-      };
-      default_connect = lib.mkOption {
-        type = lib.types.str;
-        default = "tcp://%h:%p";
-        description = ''
-        '';
-      };
-      hosts = lib.mkOption {
-        type = lib.types.listOf lib.types.attrs;
-        description = ''
-        '';
-      };
-    };
-
-    resource = {
-      path = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
-        description = ''
-          Path to a Flux RFC 20 (R version 1) file defining resources
-          available to the cluster.
-        '';
-      };
-
-      config = lib.mkOption {
-        type = lib.types.listOf lib.types.attrs;
-        default = [];
-        description = ''
-          List of resource config entries used as an alternative to
-          the RFC 20 file in `resource.path`.
-        '';
-      };
-
-      scheduling = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
-        description = ''
-          Path to a JSON file containing a graph definition in
-          JSON Graph Format that will amend the `scheduling` key
-          in the configured RFC 20 resource definition.
-        '';
-      };
-
-      exclude = lib.mkOption {
-        type = lib.types.str;
-        default = "";
-        description = ''
-          Exclude nodes matching this string from job assignment.
-        '';
-      };
-
-      norestrict = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = ''
-          Disable restricting of the loaded HWLOC topology XML to
-          the current cpu affinity mask of the Flux broker.
-        '';
-      };
-
-      noverify = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = ''
-          Disable restricting of the loaded HWLOC topology XML to the
-          current cpu affinity mask of the Flux broker.
-        '';
-      };
-
-      rediscover = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = ''
-          If true, force rediscovery of resources using HWLOC, rather
-          then using the R and HWLOC XML from the enclosing instance.
-        '';
-      };
-
-      journal-max = lib.mkOption {
-        type = lib.types.int;
-        default = 100000;
-        description = ''
-          An integer containing the maximum number of resource eventlog
-          events held in the resource module for the resource.journal RPC.
-        '';
-      };
-    };
-
-    access = {
-      allow-guest-user = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Allow guest users in the system flux instance.
-        '';
-      };
-
-      allow-root-owner = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Treat root as an owner of the system flux instance.
-        '';
-      };
-    };
-
-    tbon = {
-      tcp_user_timeout = lib.mkOption {
-        type = lib.types.str;
-        default = "2m";
-        description = ''
-        '';
-      };
-    };
-
-    kvs = {
-      checkpoint-period = lib.mkOption {
-        default = "30m";
-        description = ''
-        '';
-      };
-      gc-threshold = lib.mkOption {
-        default = 100000;
-        description = ''
-        '';
-      };
-    };
-
-    ingest = {
-      validator.plugins = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ "jobspec" "feasibility" ];
-        description = ''
-        '';
-      };
-    };
-
-    job-manager = {
-      inactive-age-limit = lib.mkOption {
-        type = lib.types.str;
-        default = "7d";
-        description = ''
-          Age limit for inactive jobs within the instance.
-        '';
-      };
-    };
-
-    policy = {
-      jobspec.defaults.system.duration = lib.mkOption {
-        type = lib.types.str;
-        default = "1m";
-        description = ''
-        '';
-      };
-
-      limits = {
-        duration = lib.mkOption {
-          type = lib.types.str;
-          default = "2h";
-          description = ''
-            Maximum duration of jobs within the instance.
-          '';
-        };
-
-        job-size = {
-          max.nnodes = lib.mkOption {
-            type = lib.types.int;
-            default = 8;
-            description = ''
-              Maximum node count a job can request.
-            '';
-          };
-          max.ncores = lib.mkOption {
-            type = lib.types.int;
-            default = 32;
-            description = ''
-              Maximum core count a job can request.
-            '';
-          };
-        };
-      };
-    };
-
-    sched-fluxion-qmanager.queue-policy = lib.mkOption {
-      type = lib.types.str;
-      default = "easy";
-      description = ''
-        TODO
-      '';
-    };
-
-    sched-fluxion-resource = {
-      match-policy = lib.mkOption {
-        type = lib.types.str;
-        default = "lonodex";
-        description = ''
-          TODO
-        '';
-      };
-      match-format = lib.mkOption {
-        type = lib.types.str;
-        default = "rv1_nosched";
-        description = ''
-          TODO
-        '';
-      };
-    };
-  };
-
-  securitySignOptions = {
-    sign.max-ttl = lib.mkOption {
-      type = lib.types.int;
-      default = 1209600; # two weeks
-      description = ''
-        TODO
-      '';
-    };
-
-    sign.default-type = lib.mkOption {
-      type = lib.types.str;
-      default = "munge";
-      description = ''
-        TODO
-      '';
-    };
-
-    sign.allowed-types = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ "munge" ];
-      description = ''
-        TODO
-      '';
-    };
-  };
-
-  securityImpOptions = {
-    exec.allowed-users = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ "flux" ];
-      description = ''
-        Users allowed to execute flux-imp.
-        WARNING: don't put normal users here!
-      '';
-    };
-
-    exec.allowed-shells = lib.mkOption {
-      type = lib.types.listOf lib.types.path;
-      default = [ "/run/wrappers/bin/flux-shell" ];
-      description = ''
-        Allowed job shells.
-        WARNING: don't put any old shell here!
-      '';
-    };
-
-    exec.pam-support = lib.mkEnableOption "pam-support";
-  };
-
 in
 {
   meta.maintainers = [ lib.maintainers.aware70 ];
@@ -349,7 +39,7 @@ in
       };
 
       system.settings = lib.mkOption {
-        type = lib.types.submodule { options = systemOptions; };
+        type = lib.types.submodule (import ./system-settings.nix);
         default = {};
         description = ''
           Flux system configuration.
@@ -358,7 +48,7 @@ in
       };
 
       security.sign.settings = lib.mkOption {
-        type = lib.types.submodule { options = securitySignOptions; };
+        type = lib.types.submodule (import ./security-sign-settings.nix);
         default = {};
         description = ''
           Flux security signing configuration. This attribute set is converted into a TOML file.
@@ -367,7 +57,7 @@ in
       };
 
       security.imp.settings = lib.mkOption {
-        type = lib.types.submodule { options = securityImpOptions; };
+        type = lib.types.submodule (import ./security-imp-settings.nix);
         default = {};
         description = ''
           Flux security IMP configuration. This attribute set is converted into a TOML file.
